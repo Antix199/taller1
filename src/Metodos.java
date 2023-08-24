c83e461203de20409295c686d1efc62aaaa9f2f0
anton <a.paredes9@ufromail.cl>
2023-08-24 10:11:32 -0400

    Avance 1

--- src/Metodos.java
+++ src/Metodos.java
@@ -1,6 +1,28 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Metodos {
+
+    static boolean validarDimensiones(int filas, int cols) {
+        return filas > 0 && cols > 0;
+    }
+
+    static int[][] crearMatriz(int filas, int cols) {
+        return new int[filas][cols];
+    }
+
+    static int generarRandom() {
+        Random random = new Random();
+        return random.nextInt(10);}
+
+    static void llenarMatriz(int matriz[][]) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = generarRandom();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,6 +35,7 @@ public class Metodos {
         if (validarDimensiones(filas, cols)) {
             int[][] matriz = crearMatriz(filas, cols);
             System.out.println("Matriz creada correctamente:");
+            llenarMatriz(matriz);
             imprimirMatriz(matriz);
         } else {
             System.out.println("Dimensiones no válidas. Deben ser números enteros positivos.");
@@ -21,18 +44,10 @@ public class Metodos {
         scanner.close();
     }
 
-    static boolean validarDimensiones(int filas, int cols) {
-        return filas > 0 && cols > 0;
-    }
-
-    static int[][] crearMatriz(int filas, int cols) {
-        return new int[filas][cols];
-    }
-
     static void imprimirMatriz(int[][] matriz) {
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + " ");
+        for (int[] fila : matriz) {
+            for (int elemento : fila) {
+                System.out.print(elemento + " ");
             }
             System.out.println();
         }
