8fa2ffee6b0d08a8c6f6f1cbea9fd289a2a3db14
anton <a.paredes9@ufromail.cl>
2023-08-24 10:46:32 -0400

    Avance 3

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,90 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Menu {
 
+    static boolean validarDimensiones(int filas, int cols) {
+        return filas > 0 && cols > 0;
+    }
+
+    static int[][] crearMatriz(int filas, int cols) {
+        return new int[filas][cols];
+    }
+
+    static int generarRandom() {
+        return new Random().nextInt(10);
+    }
+
+    static void llenarMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++)
+            for (int j = 0; j < matriz[i].length; j++)
+                matriz[i][j] = generarRandom();
+    }
+
+    static void imprimirMatriz(int[][] matriz) {
+        for (int[] fila : matriz) {
+            for (int elemento : fila) System.out.print(elemento + " ");
+            System.out.println();
+        }
+    }
+
+    static void mostrarFila(int[][] matriz, int fila) {
+        if (fila >= 0 && fila < matriz.length) {
+            System.out.println("Fila " + fila + ":");
+            for (int elemento : matriz[fila]) System.out.print(elemento + " ");
+            System.out.println();
+        } else System.out.println("Fila no válida.");
+    }
+
+    static boolean matrizCero(int[][] matriz) {
+        int total = matriz.length * matriz[0].length, ceros = 0;
+        for (int[] fila : matriz)
+            for (int elemento : fila) if (elemento == 0) ceros++;
+        return (ceros * 2) > total;
+    }
+
+    static void menu(int[][] matriz) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("Menú:\n1. Mostrar una fila\n2. Verificar si la matriz es de tipo cero\n3. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese el número de fila que desea mostrar: ");
+                    mostrarFila(matriz, scanner.nextInt());
+                    break;
+                case 2:
+                    System.out.println("¿La matriz es de tipo cero? " + matrizCero(matriz));
+                    break;
+                case 3:
+                    System.out.println("Saliendo del programa.");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcion != 3);
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el número de filas que desea crear: ");
+        int filas = scanner.nextInt();
+        System.out.print("Ingrese el número de columnas que desea crear: ");
+        int cols = scanner.nextInt();
+        if (validarDimensiones(filas, cols)) {
+            int[][] matriz = crearMatriz(filas, cols);
+            System.out.println("Matriz creada correctamente:");
+            imprimirMatriz(matriz);
+            System.out.print("Ingrese 1 para llenar la matriz: ");
+            if (scanner.nextInt() == 1) llenarMatriz(matriz);
+            else System.out.println("Su matriz quedará vacía");
+            imprimirMatriz(matriz);
+            menu(matriz);
+        } else System.out.println("Dimensiones no válidas. Deben ser números enteros positivos.");
+        scanner.close();
+    }
 }
+
--- src/Metodos.java
+++ src/Metodos.java
@@ -22,7 +22,31 @@ public class Metodos {
             }
         }
     }
+    static void mostrarFila(int matriz[][], int fila) {
+        if (fila >= 0 && fila < matriz.length) {
+            System.out.println("Fila " + fila + ":");
+            for (int elemento : matriz[fila]) {
+                System.out.print(elemento + " ");
+            }
+            System.out.println();
+        } else {
+            System.out.println("Fila no válida.");
+        }
+    }
 
+    static boolean matrizCero(int matriz[][]) {
+        int totalElementos = matriz.length * matriz[0].length;
+        int ceros = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == 0) {
+                    ceros++;
+                }
+            }
+        }
+        return (ceros * 2) > totalElementos;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -35,6 +59,7 @@ public class Metodos {
         if (validarDimensiones(filas, cols)) {
             int[][] matriz = crearMatriz(filas, cols);
             System.out.println("Matriz creada correctamente:");
+            imprimirMatriz(matriz);
 
             System.out.print("Ingrese 1 para llenar la matriz: ");
             int llenar = scanner.nextInt();
@@ -48,6 +73,7 @@ public class Metodos {
         } else {
             System.out.println("Dimensiones no válidas. Deben ser números enteros positivos.");
         }
+
 
         scanner.close();
     }
