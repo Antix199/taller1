608d4d8b4690f08fcaa2554026229488570b7ac6
anton <a.paredes9@ufromail.cl>
2023-08-24 09:31:59 -0400

    Avance 1

--- src/Metodos.java
+++ src/Metodos.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
+
 public class Metodos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el número de filas que desea crear: ");
+        int filas = scanner.nextInt();
+
+        System.out.print("Ingrese el número de columnas que desea crar: ");
+        int cols = scanner.nextInt();
+
+        if (validarDimensiones(filas, cols)) {
+            int[][] matriz = crearMatriz(filas, cols);
+            System.out.println("Matriz creada correctamente:");
+            imprimirMatriz(matriz);
+        } else {
+            System.out.println("Dimensiones no válidas. Deben ser números enteros positivos.");
+        }
+
+        scanner.close();
+    }
+
+    static boolean validarDimensiones(int filas, int cols) {
+        return filas > 0 && cols > 0;
+    }
+
+    static int[][] crearMatriz(int filas, int cols) {
+        return new int[filas][cols];
+    }
+
+    static void imprimirMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
